d1abc230a14e7f9c13533f1edac22d8ea9326d95
Gabriel Fabiano <gabriel.fabiano.souza@usp.br>
2020-10-29 08:43:30 -0300

    update

--- runtest.py
+++ runtest.py
@@ -1,10 +1,10 @@
-import bagpipes as pipes
-import numpy as np
 import itertools
 import pickle
+
+import bagpipes as pipes
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 filter_list_splus = ['filters/F0378.dat',
                      'filters/F0395.dat',
@@ -24,7 +24,6 @@ filter_list_splus_broad = ['filters/uJAVA.dat',
                            'filters/rSDSS.dat',
                            'filters/iSDSS.dat',
                            'filters/zSDSS.dat']
-
 
 # making a grid of models with different redshifts
 def exp_galaxy_grid(avs=np.array([0.0]), ages=np.array([0.0]), taus=np.array([0.0]), redshifts=np.array([0.0])):
@@ -68,19 +67,19 @@ def exp_galaxy_grid(avs=np.array([0.0]), ages=np.array([0.0]), taus=np.array([0.
 
 def galaxy_fit(galaxy_model, galaxy_properties, fit_instructions,i):
     def load_data(id):
-        if id == "galaxy"+str(i):
+        if id == "galaxy" + str(i):
             return np.array([galaxy_model.photometry, 0.1 * galaxy_model.photometry]).transpose()
 
     # making galaxy objects with models to create synthetic galaxies
-    galaxy_to_fit = pipes.galaxy("galaxy"+str(i), load_data, filt_list=filter_list_splus,
+    galaxy_to_fit = pipes.galaxy("galaxy" + str(i), load_data, filt_list=filter_list_splus,
                                  phot_units=galaxy_model.phot_units, spectrum_exists=False)
     fit = pipes.fit(galaxy_to_fit, fit_instructions, run="exp_sfh")
 
     fit.fit(verbose=False)
     print('(av,age,tau,redshift)', galaxy_properties)
-    fig = fit.plot_spectrum_posterior(save=True, show=True)
-    fig = fit.plot_sfh_posterior(save=True, show=True)
-    fig = fit.plot_corner(save=True, show=True)
+    fig = fit.plot_spectrum_posterior(save=True, show=False)
+    fig = fit.plot_sfh_posterior(save=True, show=False)
+    fig = fit.plot_corner(save=True, show=False)
 
     fig = plt.figure(figsize=(12, 7))
     gs = mpl.gridspec.GridSpec(7, 4, hspace=3., wspace=0.1)
@@ -98,33 +97,33 @@ def galaxy_fit(galaxy_model, galaxy_properties, fit_instructions,i):
     for i in range(4):
         axes.append(plt.subplot(gs[4:, i]))
         pipes.plotting.hist1d(post_quantities[labels[i]], axes[-1], smooth=True, label=labels[i])
-    plt.savefig('results/'+str(i)+'.pdf')
+    plt.savefig('pipes/plots/exp_sfh/galaxy' + str(i) + '_mix.png')
 
 def galaxy_broad_fit(galaxy_model, galaxy_properties, fit_instructions,i):
-        def load_data_broad(id):
-            if id == "galaxy_broad"+str(i):
-                return np.array([galaxy_model.photometry, 0.1 * galaxy_model.photometry]).transpose()[-5:]
+    def load_data_broad(id):
+        if id == "galaxy_broad" + str(i):
+            return np.array([galaxy_model.photometry, 0.1 * galaxy_model.photometry]).transpose()[-5:]
 
-        galaxy_to_fit_broad = pipes.galaxy("galaxy_broad"+str(i), load_data_broad, filt_list=filter_list_splus_broad,
-                                 phot_units=galaxy_model.phot_units, spectrum_exists=False)
+    galaxy_to_fit_broad = pipes.galaxy("galaxy_broad" + str(i), load_data_broad, filt_list=filter_list_splus_broad,
+                                       phot_units=galaxy_model.phot_units, spectrum_exists=False)
 
-        fit = pipes.fit(galaxy_to_fit_broad, fit_instructions, run="exp_sfh_broad")
-        print('(av,age,tau,redshift)',galaxy_properties)
-        fit.fit(verbose=True)
+    fit = pipes.fit(galaxy_to_fit_broad, fit_instructions, run="exp_sfh_broad")
+    print('(av,age,tau,redshift)', galaxy_properties)
+    fit.fit(verbose=False)
 
-        fig = fit.plot_spectrum_posterior(save=True, show=True)
-        fig = fit.plot_sfh_posterior(save=True, show=True)
-        fig = fit.plot_corner(save=True, show=True)
+    fig = fit.plot_spectrum_posterior(save=True, show=False)
+    fig = fit.plot_sfh_posterior(save=True, show=False)
+    fig = fit.plot_corner(save=True, show=False)
 
-        fig = plt.figure(figsize=(12, 7))
-        gs = mpl.gridspec.GridSpec(7, 4, hspace=3., wspace=0.1)
+    fig = plt.figure(figsize=(12, 7))
+    gs = mpl.gridspec.GridSpec(7, 4, hspace=3., wspace=0.1)
 
-        ax1 = plt.subplot(gs[:4, :])
+    ax1 = plt.subplot(gs[:4, :])
 
-        pipes.plotting.add_observed_photometry(fit.galaxy, ax1, zorder=10)
-        pipes.plotting.add_photometry_posterior(fit, ax1)
+    pipes.plotting.add_observed_photometry(fit.galaxy, ax1, zorder=10)
+    pipes.plotting.add_photometry_posterior(fit, ax1)
 
-        labels = ["sfr", "exponential:massformed", "dust:Av", "exponential:metallicity"]
+    labels = ["sfr", "exponential:massformed", "dust:Av", "exponential:metallicity"]
 
         post_quantities = dict(zip(labels, [fit.posterior.samples[l] for l in labels]))
 
@@ -132,24 +131,24 @@ def galaxy_broad_fit(galaxy_model, galaxy_properties, fit_instructions,i):
         for i in range(4):
             axes.append(plt.subplot(gs[4:, i]))
             pipes.plotting.hist1d(post_quantities[labels[i]], axes[-1], smooth=True, label=labels[i])
-        plt.savefig('results/'+str(i)+'_broad.pdf')
+    plt.savefig('pipes/plots/exp_sfh_broad/galaxy' + str(i) + '_broad_mix.png')
 
 def galaxy_sint_fit(galaxy_model, galaxy_properties, fit_instructions,i):
     def load_data_sint(id):
-        if id == "galaxy_sint"+str(i):
+        if id == "galaxy_sint" + str(i):
             for j in range(0, len(filter_list_splus)):
                 galaxy_model.photometry[j] = np.random.normal(galaxy_model.photometry[j],
                                                               2 * 0.1 * galaxy_model.photometry[j], 1)
                 return np.array([galaxy_model.photometry, 0.1 * galaxy_model.photometry]).transpose()
 
-    galaxy_to_fit_sint = pipes.galaxy("galaxy_sint"+str(i), load_data_sint, filt_list=filter_list_splus,
+    galaxy_to_fit_sint = pipes.galaxy("galaxy_sint" + str(i), load_data_sint, filt_list=filter_list_splus,
                                       phot_units=galaxy_model.phot_units, spectrum_exists=False)
     fit = pipes.fit(galaxy_to_fit_sint, fit_instructions, run="exp_sfh_sint")
     print('(av,age,tau,redshift)', galaxy_properties)
-    fit.fit(verbose=True)
-    fig = fit.plot_spectrum_posterior(save=True, show=True)
-    fig = fit.plot_sfh_posterior(save=True, show=True)
-    fig = fit.plot_corner(save=True, show=True)
+    fit.fit(verbose=False)
+    fig = fit.plot_spectrum_posterior(save=True, show=False)
+    fig = fit.plot_sfh_posterior(save=True, show=False)
+    fig = fit.plot_corner(save=True, show=False)
 
     fig = plt.figure(figsize=(12, 7))
     gs = mpl.gridspec.GridSpec(7, 4, hspace=3., wspace=0.1)
@@ -167,25 +166,26 @@ def galaxy_sint_fit(galaxy_model, galaxy_properties, fit_instructions,i):
     for i in range(4):
         axes.append(plt.subplot(gs[4:, i]))
         pipes.plotting.hist1d(post_quantities[labels[i]], axes[-1], smooth=True, label=labels[i])
-    plt.savefig('results/'+str(i)+'_sint.pdf')
+    plt.savefig('pipes/plots/exp_sfh_sint/galaxy' + str(i) + '_sint_mix.png')
+
 
 #####################################################################################################################
-#Creating models that will be used to characterization and synthetic fitting
+# Creating models that will be used to characterization and synthetic fitting
 
-ages = np.array([0.1,6.,10])
+ages = np.array([0.01, 0.1, 1., 10])
 taus = np.array([4.])
-avs = np.array([0.])#, 0.2, 0.4, 0.6, 0.8, 1.])
-redshifts = np.array([0.03])#, 0.002, 0.5])
+avs = np.array([0.])  # , 0.2, 0.4, 0.6, 0.8, 1.])
+redshifts = np.array([0.02, 0.05])  # , 0.002, 0.5])
 
-exp_galaxy_grid(avs,ages,taus,redshifts)
+# exp_galaxy_grid(avs,ages,taus,redshifts)
 #####################################################################################################################
 
-#defining fit parameteres
+# defining fit parameteres
 exp = {}
-exp["age"] = (0.1, 15.)
+exp["age"] = (0.001, 15.)
 exp["tau"] = (0.3, 10.)
-exp["massformed"] = (1., 15.) #10
-exp["metallicity"] = (0., 2.5) #0.02
+exp["massformed"] = (1., 15.)  # 10
+exp["metallicity"] = (0., 2.5)  # 0.02
 
 dust = {}
 dust["type"] = "Calzetti"
@@ -196,20 +196,19 @@ nebular = {}
 nebular["logU"] = -3.
 
 fit_instructions = {}
-fit_instructions["redshift"] = 0.03# (0.,10.)
+fit_instructions["redshift"] = 0.03  # (0.,10.)
 fit_instructions["exponential"] = exp
 fit_instructions["dust"] = dust
 fit_instructions["nebular"] = nebular
 #####################################################################################################################
-galaxy_list=pickle.load(file=open('exp_model_grid.h5', 'rb')) #loading galaxy models
+galaxy_list = pickle.load(file=open('exp_model_grid.h5', 'rb'))  # loading galaxy models
 
-for i in range(0,len(galaxy_list['properties'])):
-#i=0
-    #print(i)
-    galaxy_fit(galaxy_list['model'][i], galaxy_list['properties'][i], fit_instructions,i)
-    galaxy_broad_fit(galaxy_list['model'][i], galaxy_list['properties'][i], fit_instructions,i)
-    galaxy_sint_fit(galaxy_list['model'][i], galaxy_list['properties'][i], fit_instructions,i)
+# for i in range(0,len(galaxy_list['properties'])):
+i = 2
+galaxy_fit(galaxy_list['model'][i], galaxy_list['properties'][i], fit_instructions, i)
 
-#criar modelo com ruidos variados e ajustar - criar para redshift entre 0.002 e 0.5
-#calcular redshift máximo para que halpha caia na banda de halfapha e para linha do o3, hBeta
-#linhas de emissão/absorção - pesquisar
+# galaxy_broad_fit(galaxy_list['model'][i], galaxy_list['properties'][i], fit_instructions,i)
+# galaxy_sint_fit(galaxy_list['model'][i], galaxy_list['properties'][i], fit_instructions,i)
+
+# rodar 10 vezes pra uma galáxia normal e com ruído
+#mudar rúidos e rodar novamente
